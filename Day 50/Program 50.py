0c21939b467ff8ea8bb483b8d8bff0869e972931
Deep Shahane <61946392+Deep-De-coder@users.noreply.github.com>
2023-11-04 23:41:37 -0400

    Update Program 50.py

--- Day 50/Program 50.py
+++ Day 50/Program 50.py
@@ -13,4 +13,12 @@ class Solution:
         
         # Reject: impossible to make triangle
         return 0
-        
+'''
+Logic:
+
+Sort the List to get the top 3 lengths
+Check if the largest length is less than sum of other two
+3. If 2 is false, drop the max length take next 3 largest length and repeat 1-2
+if 2 is true, return sum of all lengths
+if loop ends, and no possible combination found, return 0
+'''
